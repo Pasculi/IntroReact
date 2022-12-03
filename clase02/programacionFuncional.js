00f7f76eda8977d45fd68ad3ce839ab6e1445d3b
Pasculi <jorge.sepulveda.turpie@gmail.com>
2022-12-03 19:53:21 -0300

    Agregando ejemplos REDUCE

--- clase02/programacionFuncional.js
+++ clase02/programacionFuncional.js
@@ -47,7 +47,7 @@ const users = [
         "firstName":"Rudi",
         "lastName":"Droset",
         "picture":"http://randomuser.me/api/portraits/men/83.jpg",
-        "account": 0
+        "account": 11
     }
 ]
 
@@ -70,29 +70,65 @@ const getFirstNameUsers2 = () => users.map((user, i ) => {
 
 console.log(getFirstNameUsers2())
 */
-const hobbies = ['Leer','Cantar', 'Reir', 'Andar en Bici', 'Trotar']
+/* const hobbies = ['Leer','Cantar', 'Reir', 'Andar en Bici', 'Trotar']
 
 const iterarHobbies = () => hobbies.map((hobbie, i) => console.log(`${hobbie} en mi casa`));
 
-iterarHobbies();
+iterarHobbies(); */
 
 //Usando Filter
 
-const ages = () => users.filter((user, i) => (user.age === 38))
+/* const ages = () => users.filter((user, i) => (user.age === 38))
 
-console.log(ages())
+console.log(ages()) */
 
 //Otro Ejemplo
 
-const getThePersonwithNoMoney = () => users.filter((user, i) =>  user.account === 0);
+//const getThePersonwithNoMoney = () => users.filter((user, i) =>  user.account === 0);
 
 
 //Creamo una copia de nuestro array le agregamos un index y un pais que se lo enviaremos por parametro en la función
 //const getOnlyUsersAccount = (country) => users.map((user, i) =>  ({...user, index: i, country:country}));
 
 //Prueba
-const getOnlyUsersAccount = (country) => users.map((user, i) =>  ({nombre:`${user.firstName} ${user.lastName}`,index: i, saldo:user.account}));
+/* const getOnlyUsersAccount = (country) => users.map((user, i) =>  ({nombre:`${user.firstName} ${user.lastName}`,index: i, saldo:user.account}));
 
 console.log(getThePersonwithNoMoney())
 
-console.log(getOnlyUsersAccount('Chile'))
+console.log(getOnlyUsersAccount('Chile')) */
+
+//Uo de REDUCE
+//Acepta dos parametros un Callback, el primer parámetro es un acumulador y el segundo es el item que se quiere sumar u operar
+
+const strings = ['Mi','nombre','es','Jorge']
+
+const usoReduce = () => users.reduce((acc, item) => {
+    acc += item.account;
+    return acc;
+}, 0);//=>Valor inicial
+
+console.log(`El promedio de las cuentas es de: $${usoReduce()/users.length} pesos Chilenos`) 
+
+const reducStrings = () => strings.reduce((acc, item)=>{
+    return acc += ' ' + item
+}, 'Hola!')//=>Valor inicial
+console.log(reducStrings());
+
+//Quién tiene mas plata en la cuentas
+
+/* const accountMax = () => users.reduce((acc, item)=>{
+    return Math.max(acc, item.account)
+}) */
+const accountMax = () => users.map((user)=>{
+    return user.account
+})
+
+const computoMax = () => accountMax().reduce((acc, item)=>{
+    return Math.max(acc, item)
+})
+const computoMin = () => accountMax().reduce((acc, item)=>{
+    return Math.min(acc, item)
+})
+
+console.log(computoMax())
+console.log(computoMin())
